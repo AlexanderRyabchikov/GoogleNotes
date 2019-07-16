fb93ca3a06fe276a40af0ba140a18e18af7ba0d3
Skender <alexasp3@gmail.com>
2019-07-16 14:18:27 +0300

    выстороена первоначалальная иерархия проекта

---
+++ app/src/main/java/com/client/googlenotes/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.client.googlenotes.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.client.googlenotes.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/client/googlenotes/ui/splash/Splash.kt
@@ -0,0 +1,13 @@
+package com.client.googlenotes.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.client.googlenotes.R
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
