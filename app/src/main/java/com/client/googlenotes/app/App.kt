d187363da6a069b987ef36d0ecf3491b6d6a2697
Skender <alexasp3@gmail.com>
2019-07-22 17:33:57 +0300

    первый билд

--- app/src/main/java/com/client/googlenotes/app/App.kt
+++ app/src/main/java/com/client/googlenotes/app/App.kt
@@ -1,10 +1,15 @@
 package com.client.googlenotes.app
 
+import android.support.multidex.BuildConfig
 import android.support.multidex.MultiDexApplication
+import com.client.googlenotes.app.module.ApiModule
+import com.client.googlenotes.app.module.AppModule
+import com.client.googlenotes.dagger.core.AppComponent
+import com.client.googlenotes.dagger.core.DaggerAppComponent
 
 class App : MultiDexApplication() {
 
-    private lateinit var appComponent:AppComponent
+    private lateinit var appComponent: AppComponent
 
 
     override fun onCreate() {
@@ -16,8 +21,8 @@ class App : MultiDexApplication() {
     private fun initComponents() {
         appComponent = DaggerAppComponent.builder()
             .appModule(AppModule(this))
-            .apiModule(ApiModule(BuildConfig.ServerUrl + BuildConfig.API))
-            .build();
+            .apiModule(ApiModule(/*BuildConfig.ServerUrl + BuildConfig.API*/))
+            .build()
     }
 
     fun getAppComponent() :AppComponent = appComponent
--- app/src/main/java/com/client/googlenotes/app/module/AppModule.kt
+++ app/src/main/java/com/client/googlenotes/app/module/AppModule.kt
@@ -1,7 +1,15 @@
 package com.client.googlenotes.app.module
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
-class AppModule {
+class AppModule (private val context: Context) {
+
+    @Provides
+    @Singleton
+    fun provideContext(): Context = context
+
 }
